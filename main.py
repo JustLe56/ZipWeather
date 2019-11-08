d66c93a6af2b95a8f8262d1195e8d6af819a10b7
Justin Le <justinle105@gmail.com>
2019-11-07 23:46:14 -0800

    updated longitude and latitude getting
    more efficient getting object data directly instead of making new object array based and finding specific index

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from bs4 import BeautifulSoup
 from uszipcode import SearchEngine
 import requests
 
-def getData(locationUrl):
+
+#functions to get various data from weather.gov using bs4
+def getData(locationUrl): #gets the main soup object
 	res = requests.get(locationUrl) #gets all of html
 	soup = BeautifulSoup(res.text,features="html.parser") #parses html w/ bs4
 	return soup
@@ -18,29 +20,28 @@ def getCurrentTempF(locationUrl):
 def getCurrentTempC(locationUrl):
 	elems = getData(locationUrl).select('#current_conditions-summary > p.myforecast-current-sm')
 	return elems[0].text.strip()
+
 def getTomorrowDayForecast(locationUrl):
 	elems = getData(locationUrl).select('#detailed-forecast-body > div:nth-child(3) > div.col-sm-10.forecast-text')
 	return elems[0].text.strip()
 
 
 goAgain = 'Y'
-while goAgain == 'Y' or goAgain == 'y':
+while goAgain == 'Y' or goAgain == 'y': #while loop allows users to enter new zipcodes
 	try:
 		givenzipCode = input('Enter a zipcode: ')
 		print('Loading weather info...\n')
-		
 		search = SearchEngine(simple_zipcode=True) # set simple_zipcode=False to use rich info database
 		zipcode = search.by_zipcode(givenzipCode)
-		zipInfo = zipcode.values()
-		givenLat = str(zipInfo[7])
-		givenLng = str(zipInfo[8])
+		
+		givenLat = str(zipcode.lat)
+		givenLng = str(zipcode.lng)
 
 		url = 'https://forecast.weather.gov/MapClick.php?lat='+givenLat+'&lon='+givenLng
-
 		print('The current temperature is '+ getCurrentTempF(url) + ' or '+getCurrentTempC(url) + ' in ' + getLocationName(url))
 		print('The forecast for tomorrow is "' + getTomorrowDayForecast(url) + '"')
-		goAgain = input('Try another zipcode? (Y/N): ')
-	except IndexError:
+		goAgain = input('\nTry another zipcode? (Y/N): ')
+	except IndexError: #handles if given zipcode doesn't exist
 		print('Could not find zipcode. Please try again with a different zipcode')
 		goAgain = input('\nTry another zipcode? (Y/N): ')
 
