d8f152d1ad88c5dec6a18b16653fa575601b10c6
Justin Le <justinle105@gmail.com>
2019-12-05 16:27:08 -0800

    added city functionality
    has option for user to choose to input city or zipcode

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from bs4 import BeautifulSoup
 from uszipcode import SearchEngine
 import requests
+import sys, os
+
+#used for handling library print statements
+# Disable printing
+def blockPrint():
+    sys.stdout = open(os.devnull, 'w')
+
+# Restores printing
+def enablePrint():
+    sys.stdout = sys.__stdout__
 
 
 #functions to get various data from weather.gov using bs4
@@ -21,27 +31,58 @@ def getCurrentTempC(locationUrl):
 	elems = getData(locationUrl).select('#current_conditions-summary > p.myforecast-current-sm')
 	return elems[0].text.strip()
 
-def getTomorrowDayForecast(locationUrl):
-	elems = getData(locationUrl).select('#detailed-forecast-body > div:nth-child(3) > div.col-sm-10.forecast-text')
+def getFirstForecastTitle(locationUrl):
+	elems = getData(locationUrl).select('#detailed-forecast-body > div:nth-child(1) > div.col-sm-2.forecast-label')
 	return elems[0].text.strip()
 
+def getFirstForecastInfo(locationUrl):
+	elems = getData(locationUrl).select('#detailed-forecast-body > div:nth-child(1) > div.col-sm-10.forecast-text')
+	return elems[0].text.strip()
+
+def getSecondForecastTitle(locationUrl):
+	elems = getData(locationUrl).select('#detailed-forecast-body > div:nth-child(2) > div.col-sm-2.forecast-label')
+	return elems[0].text.strip()
+
+def getSecondForecastInfo(locationUrl):
+	elems = getData(locationUrl).select('#detailed-forecast-body > div:nth-child(2) > div.col-sm-10.forecast-text')
+	return elems[0].text.strip()
+
+def output(givenLat,givenLng):
+	url = 'https://forecast.weather.gov/MapClick.php?lat='+givenLat+'&lon='+givenLng
+	print('The current temperature is '+ getCurrentTempF(url) + ' or '+getCurrentTempC(url) + ' in ' + getLocationName(url))
+	print('\n The forecast for '+getFirstForecastTitle(url)+' is "' + getFirstForecastInfo(url) + '"')
+	print('\n The forecast for '+getSecondForecastTitle(url)+' is "' + getSecondForecastInfo(url)+ '"')
 
 goAgain = 'Y'
 while goAgain == 'Y' or goAgain == 'y': #while loop allows users to enter new zipcodes
-	try:
-		givenzipCode = input('Enter a zipcode: ')
-		print('Loading weather info...\n')
-		search = SearchEngine(simple_zipcode=True) # set simple_zipcode=False to use rich info database
-		zipcode = search.by_zipcode(givenzipCode)
-		
-		givenLat = str(zipcode.lat)
-		givenLng = str(zipcode.lng)
+	choice = input("City(C) or Zipcode(Z): ")
+	if(choice == 'Z'):
+		try:
+			givenzipCode = input('Enter a zipcode: ')
+			print('Loading weather info...\n')
+			search = SearchEngine(simple_zipcode=True) # set simple_zipcode=False to use rich info database
+			zipcode = search.by_zipcode(givenzipCode)
+			
+			givenLat = str(zipcode.lat)
+			givenLng = str(zipcode.lng)
 
-		url = 'https://forecast.weather.gov/MapClick.php?lat='+givenLat+'&lon='+givenLng
-		print('The current temperature is '+ getCurrentTempF(url) + ' or '+getCurrentTempC(url) + ' in ' + getLocationName(url))
-		print('The forecast for tomorrow is "' + getTomorrowDayForecast(url) + '"')
-		goAgain = input('\nTry another zipcode? (Y/N): ')
-	except IndexError: #handles if given zipcode doesn't exist
-		print('Could not find zipcode. Please try again with a different zipcode')
-		goAgain = input('\nTry another zipcode? (Y/N): ')
+			output(givenLat,givenLng)
+			goAgain = input('\nTry another city/zipcode? (Y/N): ')
+		except IndexError: #handles if given zipcode doesn't exist
+			print('Could not find zipcode. Please try again with a different zipcode')
+			goAgain = input('\nTry another city/zipcode? (Y/N): ')
+	elif(choice == 'C'):
+		try:
+			givenName = input('Enter a city name: ')
+			print('Loading weather info...\n')
+			search = SearchEngine(simple_zipcode=True) # set simple_zipcode=False to use rich info database
+			city = search.by_city(givenName)
+			zipcode = city[0]
+			givenLat = str(zipcode.lat)
+			givenLng = str(zipcode.lng)
 
+			output(givenLat,givenLng)
+			goAgain = input('\nTry another city/zipcode? (Y/N): ')
+		except IndexError: #handles if given zipcode doesn't exist
+			print('Could not find zipcode. Please try again with a different zipcode')
+			goAgain = input('\nTry another city/zipcode? (Y/N): ')
